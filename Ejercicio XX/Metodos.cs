e1015e8bf507da2d78837618391f5599aba3ea3c
Julian Aleksandrowicz <62268641+ju-aleksandrowicz@users.noreply.github.com>
2022-09-06 16:40:30 -0300

    Create Metodos.cs

---
+++ Ejercicio XX/Metodos.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_20
+{
+    internal class Metodos
+    {
+        public void validarNumero(string entrada, ref int salida)
+        {
+            bool flag = false;
+
+            do
+            {
+                Console.Write("Por favor ingrese un numero: ");
+                entrada = Console.ReadLine();
+
+                if (!int.TryParse(entrada, out salida))
+                {
+                    Console.WriteLine("Error! El dato ingresado no es un numero, vuelva a intentarlo.");
+                }
+                else
+                {
+                    flag = true;
+                }
+            } while (!flag);
+
+        }
+
+        public int calcularFactorial(int numero)
+        {
+            for (int i = (numero - 1); i >= 1; i--)
+            {
+                numero = numero * i;
+            }
+
+            return numero;
+        }
+
+        public bool comprobarPrimo(int numero)
+        {
+            for (int i = 2; i < numero; i++)
+            {
+                if ((numero % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
